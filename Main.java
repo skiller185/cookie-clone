e73a898053874803646e1528b283c9eee6b26c84
Anthony Carpenetti <31355951+skiller185@users.noreply.github.com>
2018-12-07 21:54:28 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -9,10 +9,11 @@ import java.awt.event.*;
 				java.awt.Window
 					java.awt.Frame
 */
-public class Main extends Frame {
+public class Main extends Frame implements ActionListener {
 
 	// private variables
 	private int cookiesValue = 0;
+	private Button bigCookie;
 	private Label cookiesTitle;
 	private TextField cookiesDisplay;
 
@@ -25,16 +26,28 @@ public class Main extends Frame {
 		cookiesTitle = new Label("Cookies");
 		add(cookiesTitle);
 
-		cookiesDisplay = new TextField(10);
+		cookiesDisplay = new TextField(cookiesValue + "", 10);
 		cookiesDisplay.setEditable(false);
 		add(cookiesDisplay);
 
+		bigCookie = new Button("Get Cookie");
+		add(bigCookie);
+
+		bigCookie.addActionListener(this);
+
+		setTitle("Cookie Clone");
 		setSize(1600, 900);
 
 		setVisible(true);
 	}
 
 	public static void main(String[] args) {
-		new Main();
+		Main program = new Main();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		++cookiesValue;
+		cookiesDisplay.setText(cookiesValue + "");
 	}
 }
